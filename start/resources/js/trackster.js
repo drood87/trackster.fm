720437230e69e8c72b1a3ebe84e43d39afc4704b
drood <dbernstein1987@hotmail.com>
2018-03-29 19:42:11 +1100

    added jQuery logic and song info header + styles

--- start/resources/js/trackster.js
+++ start/resources/js/trackster.js
@@ -1,17 +1,5 @@
-$(document).ready(function() {
-	
-	$("#submitButton").click(function() {
-		Trackster.searchTracksByTitle($("#inputField").val());
-		console.log($.ajax({
-			url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=tiny&api_key=975c4032896f16103388baa23cf193f3&format=json'
-		}))
-	})
-
-})
-
-const API_KEY = 975c4032896f16103388baa23cf193f3;
-
 var Trackster = {};
+const API_KEY = '975c4032896f16103388baa23cf193f3';
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
@@ -26,9 +14,19 @@ Trackster.renderTracks = function(tracks) {
   Render the tracks given in the API query response.
 */
 Trackster.searchTracksByTitle = function(title) {
-
+		$.ajax({
+					url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track='+($("#inputField").val())+'&api_key='+API_KEY+'&format=json',
+					success: function(data) {
+						console.log("Success!", data);
+					},
+					error: function(e) {
+						console.log("Error!", e);
+					}
+				})
 };
 
 $(document).ready(function() {
-	// body...
+	$("#submitButton").click(function() {
+		Trackster.searchTracksByTitle($("#inputField").val());
+	})
 })
